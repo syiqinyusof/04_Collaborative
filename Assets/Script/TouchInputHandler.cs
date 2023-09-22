d0756b10b4d7571b7cfca778fa5cfdef58a1ab23
syahidatulasyiqin <asyikin@netqlix.com>
2023-09-22 17:21:42 +0800

    Update touch input

---
+++ Assets/Script/TouchInputHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchInputHandler : MonoBehaviour
+{
+    public void OnButtonTouch()
+    {
+        Debug.Log("Button touched!");
+        // Add your custom touch handling logic here
+    }
+}
